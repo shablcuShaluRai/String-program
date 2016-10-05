faff1931e20d3679e6f3d2687d2d1865d3b0d4a2
shalurai <shaluraics22@gmail.com>
2016-10-05 20:18:31 +0900

    String builder test

---
+++ src/Buffertest.java
@@ -0,0 +1,22 @@
+
+public class Buffertest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringBuffer sb=new StringBuffer("Test");
+		// creates count=4;
+		// Tocachestring creates 
+		//value contains arraysize.
+		
+		sb.append(" welcome world");
+		System.out.println(sb);
+		System.out.println(sb.capacity());
+		
+		
+	}
+
+}
---
+++ src/BuilderTest.java
@@ -0,0 +1,17 @@
+
+public class BuilderTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringBuilder sb=new StringBuilder("Hello ");  
+		sb.append("Java");//now original string is changed  
+		System.out.println(sb);
+		
+		
+	}
+
+}
